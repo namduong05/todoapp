93ee23388dd1262353c87ed7ef386ca136724893
namduong05 <chiutoe123@gmail.com>
2026-02-27 01:06:13 +0700

    chuẩn bị deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,17 +3,29 @@ import tasksRoute from "./routes/tasksRouters.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 const app = express();
 
-app.use(cors());
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors());
+}
 app.use(express.json());
 
 app.use("/api/tasks", tasksRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
